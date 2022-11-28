0724329a4219b085393c3c8e42a2c3d06a5eb7d2
margusja <margusja@mac-cy9ll9445c.lan>
2022-11-28 20:13:57 +0200

    in out with one pin

---
+++ in-out-one-button.asm
@@ -0,0 +1,24 @@
+	.include "m328pdef.inc"
+
+	.cseg
+	.org 	0x00
+	ldi	r16,0b00100000
+	out	DDRB,r16
+	sbi	PORTB,0
+
+loop1:
+	sbic 	PINB,0
+	rjmp	loop1
+	rjmp	led_on			; jump to led_on method
+
+loop2:
+	sbic 	PINB,0
+	rjmp	loop2			; stay in infinite loop
+	rjmp 	led_off			; jump to led_of method
+
+led_on:
+	sbi     PORTB,5 
+	rjmp loop2
+led_off:
+	cbi     PORTB,5 
+	rjmp 	loop1
