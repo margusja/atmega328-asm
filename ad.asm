406b68b71fec420b343073d09bf5b49dca3da3a0
margusja <margusja@mac-cy9ll9445c.lan>
2022-12-04 20:22:48 +0200

    AD conversion is stable now

--- ad.asm
+++ ad.asm
@@ -5,9 +5,14 @@
 
 ; start configuration
 
+; clear ADCSRA and ADCSRB
+ldi 	R16,0b00000000
+sts	ADCSRA,R16
+sts	ADCSRB,R16
+
 ; Using AVCC 
 ; Using A5 as an input
-ori	R16,(1<<REFS0)
+ori	R16,(1<<REFS0)|(1<<ADLAR)
 sts	ADMUX,R16
 
 ; enable ADC
@@ -15,7 +20,7 @@ sts	ADMUX,R16
 ldi	R16,(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)
 sts	ADCSRA,R16
 
-; disable free running mode
+; one capture event
 ldi 	R16,(1<<ADTS2)|(1<<ADTS1)|(1<<ADTS0)
 sts     ADCSRB,R16
 
@@ -42,8 +47,8 @@ loop:
 	rjmp	saveresult	; AD conversion is ready jump to result
 
 saveresult:
-	cbi	PORTB,4 	; set pin to indicate we are in ADC loop
-	lds	R16,ADCL
+	
+	lds	R16,ADCH
 	out	PORTD,R16
 	rjmp	result
 	
