8c5ab06b56996337f3669349d17f67d6d1e0339c
margusja <margusja@mac-cy9ll9445c.lan>
2022-12-06 21:02:30 +0200

    AD conversion using ADIF to detect if conversion is ready

--- ad-freerun.asm
+++ ad-freerun.asm
@@ -18,8 +18,8 @@ ori	R16,(1<<REFS0)|(1<<ADLAR)
 sts	ADMUX,R16
 
 ; enable ADC
-; set clock prescale 128
-ldi	R16,(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)
+; set clock prescale 16
+ldi	R16,(1<<ADEN)|(1<<ADPS2)
 sts	ADCSRA,R16
 
 ; one capture event
@@ -30,11 +30,6 @@ sts     ADCSRB,R16
 ldi	R16,0b11111111
 out 	DDRD, R16
 
-sbi 	DDRB,5    		; Set PB5 (onboard led) as Output
-sbi	DDRB,4			; DEBUG pin for ADC saveresult subroutine
-cbi	PORTB,5			; turn led off
-cbi	PORTB,4
-
 ; end of configuration
 
 ; start polling
@@ -42,19 +37,10 @@ ori	R16,(1<<ADSC)
 sts	ADCSRA,R16
 
 loop:
-	sbi	PORTB,4 	; set pin to indicate we are in ADC loop
 	lds	r16,ADCSRA
-	sbrc 	r16,ADSC
+	sbrs 	r16,ADIF
 	rjmp 	loop		; AD conversion is still progress jump back to loop
-	rjmp	saveresult	; AD conversion is ready jump to result
-
-saveresult:
-	
 	lds	R16,ADCH
 	out	PORTD,R16
-	rjmp	result
-	
-result:
-	sbi	PORTB,5		; set led on arduono board pin 13 high to indicate result is ready
 	rjmp	start		; infinite loop	
 
